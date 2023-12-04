0b11be037e47fd82a4e8e5bc66fb54cb64c959bc
Nicole Mazzuca <nicole@strega-nil.co>
2023-12-03 19:22:43 -0800

    add COPYING

--- src/day1.ml
+++ src/day1.ml
@@ -1,3 +1,20 @@
+(*
+Copyright (C) 2023 Nicole Mazzuca
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
+
 let part_1 ~(data : string list) : int =
   let parse_line (s : string) : int =
     let is_digit (c : char) : bool = '0' <= c && c <= '9' in
